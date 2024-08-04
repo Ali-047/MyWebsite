8e3d3294c4afc397c94f756286090c4b0588b3f2
AliNemati <nematiali06@gmail.com>
2024-08-04 19:23:23 +0330

    base version

---
+++ JS/Neyshabur.js
@@ -0,0 +1,36 @@
+const mainPic = document.getElementById("mainPic") ;
+const scrollingPics = document.querySelector(".scrollingPics ul") ;
+const picTitle = document.getElementById("title") ;
+const picDescription = document.getElementById("description") ;
+
+function showPics() {
+    var mainList = "" ;
+    fetch("./../cities.json")
+    .then((res) => res.json())
+    .then((data) => {
+        
+        mainPic.src = `${data[2].info[0].src}` ;
+        picTitle.innerText = data[2].info[0].name ;
+        picDescription.innerText = data[2].info[0].description ;
+        
+        for (let i = 0; i < data[2].info.length; i++) {
+            mainList += `
+            <li>
+            <img src="${data[2].info[i].src}" alt="">
+            </li>
+            `
+        }
+        scrollingPics.innerHTML = mainList ;
+
+        const picList = document.querySelectorAll(".scrollingPics ul li")
+        picList.forEach((value , index) => {
+            value.addEventListener("click" , ()=> {
+                mainPic.src = `${data[2].info[index].src}` ;
+                picTitle.innerText = data[2].info[index].name ;
+                picDescription.innerText = data[2].info[index].description ;
+            })
+        })
+    })
+}
+
+showPics() ;
